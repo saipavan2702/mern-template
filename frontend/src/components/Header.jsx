16a063fefb72ba4bf2c14afca4c52cc196f28942
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-04-08 02:33:46 +0530

    l

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,31 +1,46 @@
 import React from "react";
-import { FaSignInAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaSignInAlt, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <div className="header">
       <div className="header__logo">
         <Link to="/">Mern</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt />
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser />
-            Register
-          </Link>
-        </li>
-        {/* <li>
-          <Link to="/logout">
-            <FaSignOutAlt />
-          </Link>
-        </li> */}
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -11,6 +11,20 @@ export const register = async (userData) => {
   return res.data;
 };
 
-const authService = { register };
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const authService = { register, login, logout };
 
 export default authService;
